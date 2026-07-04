7767690622fee3b7362b37a7f8cbe1271e2b6a60
ParadiseDM <ParadiseDMX@gmail.com>
2026-07-04 11:35:01 +0300

    Обновил файл

--- shared/lib/get-available-pizza-sizes.ts
+++ shared/lib/get-available-pizza-sizes.ts
@@ -2,7 +2,7 @@ import { ProductItem } from "@prisma/client";
 import { pizzaSizes, PizzaType } from "../constants/pizza";
 import { Variant } from "../components/shared/group-variants";
 
-export const getAvailablePizzaSizes = (type: PizzaType[], items: ProductItem[]): Variant[] => {
+export const getAvailablePizzaSizes = (type: PizzaType, items: ProductItem[]): Variant[] => {
 	const filteredPizzasByType = items.filter((item) => item.pizzaType === type);
 	
 	return pizzaSizes.map((item) => ({
