5645328c2a2994dfa956eee71181b1e7463cce5b
ParadiseDM <ParadiseDMX@gmail.com>
2026-07-04 11:42:45 +0300

    Обновил файл

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config = {
-  darkMode: ['class'],
+  darkMode: 'class',
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   './pages/**/*.{js,ts,jsx,tsx,mdx}',
